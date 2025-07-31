4d62d5c76de7e15d15d2d6cbd36992654139629d
uwbdkeb <gugltablica622@gmail.com>
2025-07-31 14:48:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+
 import os
 
 # Токен от @BotFather
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # ID администратора (ваш Telegram ID)
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
